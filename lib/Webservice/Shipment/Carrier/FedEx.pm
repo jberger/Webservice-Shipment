0d309d3a507dfea4a0f157126c926e82e91a62cf
Joel Berger <joel.a.berger@gmail.com>
2017-11-13 18:17:54 -0600

    further improve formatting, switch out Cpanel::JSON::XS

--- lib/Webservice/Shipment/Carrier/FedEx.pm
+++ lib/Webservice/Shipment/Carrier/FedEx.pm
@@ -4,43 +4,22 @@ use Mojo::Base 'Webservice::Shipment::Carrier';
 
 use constant DEBUG =>  $ENV{MOJO_SHIPMENT_DEBUG};
 
-use Mojo::Template;
-use Mojo::URL;
 use Mojo::IOLoop;
+use Mojo::JSON;
+use Mojo::URL;
 use Time::Piece;
-use Cpanel::JSON::XS;
 
 has api_url => sub { Mojo::URL->new('https://www.fedex.com/trackingCal/track') };
 has carrier_description => sub { 'FedEx' };
 has validation_regex => sub {    qr/(\b96\d{20}\b)|(\b\d{15}\b)|(\b\d{12}\b)/ };
 
-has template => <<'TEMPLATE';
-% my ($self, $id) = @_;
-<?xml version="1.0" ?>
-<AccessRequest xml:lang='en-US'>
-  <AccessLicenseNumber><%== $self->api_key %></AccessLicenseNumber>
-  <UserId><%== $self->username %></UserId>
-  <Password><%== $self->password %></Password>
-</AccessRequest>
-<?xml version="1.0" ?>
-<TrackRequest>
-  <Request>
-    <TransactionReference>
-      <CustomerContext><%== $id %></CustomerContext>
-    </TransactionReference>
-    <RequestAction>Track</RequestAction>
-  </Request>
-  <TrackingNumber><%== $id %></TrackingNumber>
-</TrackRequest>
-TEMPLATE
-
 sub human_url {
-  my ($self, $id, $dom) = @_;
+  my ($self, $id, $doc) = @_;
   return Mojo::URL->new('https://www.fedex.com/apps/fedextrack/')->query(action => 'track', locale => 'en_US', cntry_code => 'us', language => 'english', tracknumbers => $id);
 }
 
 sub extract_destination {
-  my ($self, $id, $dom, $target) = @_;
+  my ($self, $id, $doc, $target) = @_;
 
   my %targets = (
     postal_code => 'destZip',
@@ -50,37 +29,37 @@ sub extract_destination {
   );
 
   my $t = $targets{$target} or return;
-  my $addr = $dom->{$t} or return;
+  my $addr = $doc->{$t} or return;
   return $addr;
 }
 
 sub extract_service {
-  my ($self, $id, $dom) = @_;
-  my $class = $dom->{'serviceDesc'};
-  my $service =  $class =~m/fedex/i ? $class : 'FedEx ' . $class;
+  my ($self, $id, $doc) = @_;
+  my $class = $doc->{'serviceDesc'};
+  my $service = $class =~ /fedex/i ? $class : 'FedEx ' . $class;
   return $service;
 }
 
 sub extract_status {
-  my ($self, $id, $dom) = @_;
+  my ($self, $id, $doc) = @_;
 
-  my $summary = $dom->{'scanEventList'}->[0];
+  my $summary = $doc->{'scanEventList'}->[0];
   return unless $summary;
 
-  my $delivered = $dom->{isDelivered} ? 1 : 0;
+  my $delivered = $doc->{isDelivered} ? 1 : 0;
 
-  my $desc = $dom->{statusWithDetails};
+  my $desc = $doc->{statusWithDetails};
   unless ($summary->{date}) {
-    $desc = 'No information found for <a href="' . human_url($id) . '">' . $id . '</a>';
+    $desc = 'No information found for <a href="' . $self->human_url($id) . '">' . $id . '</a>';
     return ($desc, undef, $delivered);
   }
+
   my $timestamp = join(' ', $summary->{date}, $summary->{time});
   eval{
     $timestamp = Time::Piece->strptime($summary->{date} . ' T ' . $summary->{time}, '%Y-%m-%d T %H:%M:%S');
   };
 
   $desc = $summary->{date} ? join(' ', $desc , $summary->{date}, $summary->{time}) : $desc;
-  $desc ||= Cpanel::JSON::XS->new->pretty(1)->encode($dom->{'scanEventList'});
   return ($desc, $timestamp, $delivered);
 }
 
@@ -91,13 +70,13 @@ sub request {
 
   my $tx = $self->ua->build_tx(
     POST => $self->api_url.
-    {Accept => '*/*'},
+    {Accept => 'application/json'},
     form => {
       action => 'trackpackages',
       locale => 'en_US',
       version => '1',
       format => 'json',
-      data => Cpanel::JSON::XS->new->encode({
+      data => Mojo::JSON::encode_json({
         TrackPackagesRequest => {
           appType => 'WTRK',
           uniqueKey => '',
@@ -149,14 +128,6 @@ Webservice::Shipment::Carrier::FedEx - FedEx handling for Webservice::Shipment
 
 Implements FedEx handling for L<Webservice::Shipment>.
 It is a subclass of L<Webservice::Shipment::Carrier> which implements all the necessary methods.
-
-=head1 ATTRIBUTES
-
-L<Webservice::Shipment::Carrier::FedEx> implements all of the attributes from L<Webservice::Shipment::Carrier> and implements the following new ones
-
-=head2 template
-
-The string template used with L<Mojo::Template> to format the request.
 
 =head1 NOTES
 
